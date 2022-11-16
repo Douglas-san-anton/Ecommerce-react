8d833193c4f0dcb250ad4ce61f4334ad323e4572
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2022-11-16 11:28:34 -0300

    navbar modification

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Styles from './Navbar.module.css'
 import logo from '../../assets/logo.svg'
+import menu from '../../assets/icon-menu.svg'
+import cart from '../../assets/icon-cart.svg'
+import avatar from '../../assets/image-avatar.png'
 
 export const Navbar = () => {
   return (
@@ -8,19 +11,27 @@ export const Navbar = () => {
 
       <div className={Styles.nav__container}>
 
-        <img className={Styles.nav__logo} src={logo} />
+        <div className={Styles.nav__flex}>
 
-        <label htmlFor="menu" className={Styles.nav__label}>
-          <img src={logo} alt="icon" className={Styles.nav__img}/>
-        </label>
-        <input type="checkbox" id="menu" className={Styles.nav__input}/>
+            <img className={Styles.nav__logo} src={logo} />
 
-        <div className={Styles.nav__menu}>
-          <a href="#" className={Styles.nav__item}>Collections</a>
-          <a href="#" className={Styles.nav__item}>Men</a>
-          <a href="#" className={Styles.nav__item}>Women</a>
-          <a href="#" className={Styles.nav__item}>About</a>
-          <a href="#" className={Styles.nav__item}>Contact</a>
+            <label htmlFor="menu" className={Styles.nav__label}>
+              <img src={menu} alt="icon" className={Styles.nav__img}/>
+            </label>
+            <input type="checkbox" id="menu" className={Styles.nav__input}/>
+
+            <div className={Styles.nav__menu}>
+              <a href="#" className={Styles.nav__item}>Collections</a>
+              <a href="#" className={Styles.nav__item}>Men</a>
+              <a href="#" className={Styles.nav__item}>Women</a>
+              <a href="#" className={Styles.nav__item}>About</a>
+              <a href="#" className={Styles.nav__item}>Contact</a>
+            </div>
+        </div>
+
+        <div className={Styles.nav__avatar}>
+          <img src={cart} alt="" className={Styles.nav__avatar_cart} />
+          <img src={avatar} alt="" className={Styles.nav__avatar_avatar}/>
         </div>
 
       </div>
